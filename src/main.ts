ba74add3b235f3ddc3d9340635bc3834f5a03ef7
Kryst <krystallizer26@gmail.com>
2022-03-03 22:57:21 +0700

    enalbe cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,12 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors({
+    "origin": "*",
+    "allowedHeaders" : "Authorization,content-type",
+    "credentials": true
+  });
+
   //Backend Setup
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
