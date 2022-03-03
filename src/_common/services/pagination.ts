b1976ebcd301e6649335c0fdf81922d2dbd21fa8
Kryst <krystallizer26@gmail.com>
2022-03-04 00:22:16 +0700

    agian

--- src/_common/services/pagination.ts
+++ src/_common/services/pagination.ts
@@ -1,5 +1,6 @@
 export function calculatePagination(pageNum: number, pageSize: number, maxCount: number) {
   let paginate = {
+    maxCount: maxCount,
     pageNum: pageNum,
     pageSize: pageSize,
     pageMax: Math.ceil(maxCount / pageSize),
